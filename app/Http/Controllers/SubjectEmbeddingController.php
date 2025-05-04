fb6be4b40c041f58a18c7082a110a146875ff18f
Danushkasupun <danushksupun1@gmail.com>
2025-05-04 21:56:00 +0530

    em

--- app/Http/Controllers/SubjectEmbeddingController.php
+++ app/Http/Controllers/SubjectEmbeddingController.php
@@ -10,7 +10,7 @@ class SubjectEmbeddingController extends Controller
     public function generate($id, OpenAIEmbeddingService $embedder)
     {
         $subject = subject::findOrFail($id);
-        $text = $subject->subject_title . ' ' . $subject->description;
+        $text = $subject->subject_title . ' ' . $subject->description . ' ' . $subject->subject_grade . ' ' . $subject->new_price;
         $embedding = $embedder->getEmbedding($text);
 
         $subject->embedding = json_encode($embedding);
