61198bb5fbfdfaaa2919bae510662f9048eae807
Danushkasupun <danushksupun1@gmail.com>
2025-05-06 23:07:30 +0530

    v

--- database/migrations/2025_05_06_172906_create_item_histories_table.php
+++ database/migrations/2025_05_06_172906_create_item_histories_table.php
@@ -12,7 +12,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('item_histories', function (Blueprint $table) {
-            $table->id();
+            $table->string('external_number',200)->nullable();
+            $table->integer('branch_id')->nullable();
+            $table->integer('location_id')->nullable();
+            $table->integer('document_number')->nullable();
+            $table->date('transaction_date')->nullable();
+            $table->string('description')->nullable();
+            $table->integer('item_id')->nullable();
+            $table->decimal('quantity',10,2)->nullable();
+            $table->decimal('free_quantity')->nullable();
+            $table->string('batch_number',200)->nullable();
+            $table->decimal('whole_sale_price',10,2)->nullable();
+            $table->decimal('retial_price',10,2)->nullable();
+            $table->date('expire_date')->nullable();
+            $table->decimal('cost_price')->nullable();
             $table->timestamps();
         });
     }
