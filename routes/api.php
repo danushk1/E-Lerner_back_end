35e1ef2f8120deeeac63145c38a28c36d42e75b1
Danushkasupun <danushksupun1@gmail.com>
2025-05-10 23:37:17 +0530

    g

--- routes/api.php
+++ routes/api.php
@@ -38,5 +38,5 @@ Route::post('/chat', [OpenAIController::class, 'chat']);
 Route::post('/embeddings/subjects', [EmbeddingController::class, 'createSubjectEmbeddings']);
 Route::get('/embedding/subject', [SubjectEmbeddingController::class, 'generate']);
 Route::post('/search/subjects', [SubjectSearchController::class, 'search']);
-//Route::post('/chart/generate', [ChartAssistantController::class, 'generate']);
-Route::post('/chart/generate', [itemhistorycontroller::class, 'generate']);
+Route::post('/chart/generate', [ChartAssistantController::class, 'generate']);
+//Route::post('/chart/generate', [itemhistorycontroller::class, 'generate']);
